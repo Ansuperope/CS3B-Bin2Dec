2332cc8a5a844d2523344ea36850637f826d246e
Ansuperope <aspenfbn@yahoo.com>
2026-03-17 22:11:22 -0700

    fixed typo in make file. Creating get input, confirmed input checks for q and c and can termiante when detecting any of them

--- Bin2Dec.s
+++ Bin2Dec.s
@@ -1,7 +1,7 @@
 // ---------------------------------------------------------------------
-// Aspen Cristobal
-// CS3b - lab-LOREM_NUM-LOREM_ASSIGN
-// mm/dd/2026
+// Aspen Cristobal & Sophia
+// CS3b - Bin2 Dec
+// 3/24/2026
 // ---------------------------------------------------------------------
 // 	PURPOSE:
 // LOREM
@@ -10,23 +10,53 @@
 // LOREM
 // ---------------------------------------------------------------------
 // 	PSUEDOCODE:
-// LOREM
+// 1. (getstring.s) Get user input
+// 2. (getstring.s) Process User input
+// 	a. get rid of non binary values (only 1 and 0 should remain)
+// 	b. make it 16 bits long
+//		i. if user entered less than 16 make the remaining 0
+// 		ii. replace null with ‘\0’
+// 3. (putstring.s) Output binary
+// 4. (putstring.s) Output arrow ->
+// 5. Do 2s complement if needed
+// 	a. Check sign
+// 		i. If neg (1) convert to 2s complement 
+// 		ii. If pos (0) skip
+// 		iii. If special case skip (Most negative)
+// 6. (Bin2dec.s) Convert binary to decimal
+// 7. (putstring.s) Output decimal
 // ---------------------------------------------------------------------
 .global _start	// Provide program starting address 
 
 // functions
-//.extern funcName
+.extern getstring	// user input
 
 _start: 
+	// SYSTEM
 	.EQU SYS_exit,  93	// exit() supervisor call code 
     
-	.text  // code section 
+	// CONSTANTS
+	.EQU IN_LEN,	64	// max input length
+	.EQU BIN_LEN,	17	// max binary string length
 
-	// terminate the program 
+	.text  // code section
+
+	// -----------------------------------------------------------------
+	// GET USER INPUT
+	// -----------------------------------------------------------------
+	LDR X0, =szInBuffer		// store input
+	MOV X1, IN_LEN			// input max length
+	BL  getstring
+
+	// -----------------------------------------------------------------
+	// TERMINATE PROGRAM
+	// -----------------------------------------------------------------
 	MOV X0, #0			// set return code to 0, all good 
 	MOV X8, #SYS_exit	// set exit() supervisor call code 
 	SVC 0				// call Linux to exit 
 
 	.data	// data section
+szInBuffer: 	.space 	IN_LEN		// holds user input, includes null
+szBinBuffer:	.space	BIN_LEN		// holds binary string, includes null
 
 .end	// end of program, optional but good practice 
---
+++ getstring.s
@@ -0,0 +1,145 @@
+// ---------------------------------------------------------------------
+// getstring.s
+// ---------------------------------------------------------------------
+// 	PURPOSE:
+// Will read a string of characters up to a specified length from the 
+// console and save it in a specified buffer as a C-String (i.e. null
+// terminated).
+// ---------------------------------------------------------------------
+//	VARAIBLES:
+//  X0: Points to the first byte of the buffer to receive the string. This must
+//      be preserved (i.e. X0 should still point to the buffer when this function
+//      returns).
+//  X1: The maximum length of the buffer pointed to by X0 (note this length
+//      should account for the null termination of the read string (i.e. C-String)
+//  LR: Must contain the return address (automatic when BL
+//      is used for the call)
+//  All AAPCS mandated registers are preserved.
+// ---------------------------------------------------------------------
+// 	PSUEDOCODE:
+// 1. Get variables from main and save them
+// 2. Get input from user
+// 3. Process input
+//	a. make sure input isnt over max length
+//	b. make last letter \0
+//  c. replace null with \0
+// 4. Output input
+// 5. Return to main
+// ---------------------------------------------------------------------
+.global getstring	// Provide program starting address 
+
+getstring: 
+	.EQU STDIN,		0	// starndard input
+	.EQU STDOUT,	1	// standard output
+	.EQU SYS_read,	63	// Linux read()
+	.EQU SYS_write, 64	// Linux write()
+	.EQU SYS_exit,  93	// exit() supervisor call code 
+
+	.text  // code section 
+	// -----------------------------------------------------------------
+	// SAVE VARIABLES FROM MAIN
+	//	X3: X0, string
+	//	X4: X1, MAX_LENGTH
+	// -----------------------------------------------------------------
+	MOV X3, X0			// X3 = X0, variable to store string
+	MOV X4, X1			// X4 = X1, MAX_LENGTH
+
+	// -----------------------------------------------------------------
+	// READ KEYBOARD
+	// -----------------------------------------------------------------
+	MOV X0, STDIN  		// file descriptor for stdin (keyboard) 
+	MOV X1, X3			// read() needs buffer pointer in X1 
+	MOV X2, X4		 	// max amount of characters to read
+	MOV X8, SYS_read 	// Linux read() system call number 
+	SVC 0				// call Linux to execute commands
+
+	// If input not valid exit (overflow)
+	CMP  X0, XZR
+	B.LT errorEnd
+
+	// -----------------------------------------------------------------
+	// PROCESS INPUT
+    //  X0: number of characters read
+	//	X3: string
+	//	X4: MAX_LENGTH
+	//	X5: counter
+	//  X6: current character
+	// -----------------------------------------------------------------
+	// INITALIZATIONS
+	MOV X5, #0			// counter = 0
+            
+forEachChar:
+    // GET CURRENT CHARACTER
+    LDRB W6, [X3, X5] 	// X6 = string[counter]
+
+	// -----------------------------------------------------------------
+	// CHECK IF COUNTER == NUMBER OF CHARACTERS READ (X0)
+	// -----------------------------------------------------------------
+	CMP  X5, X0		// counter == num of character read (X0), exit
+	B.EQ output
+
+    // -----------------------------------------------------------------
+	// CHECK IF COUNTER == IN_LEN
+	// -----------------------------------------------------------------
+	CMP  X5, X4		// counter == IN_LEN, exit
+	B.EQ output
+
+    // -----------------------------------------------------------------
+	// CHECK IF CHAR == 'q' or CHAR == 'Q' - TERMINATE PROGRAM
+	// -----------------------------------------------------------------
+    // LOWERCASE
+    CMP  W6, #'q'
+    B.EQ errorEnd
+
+    // UPPERCASE
+    CMP W6, #'Q'
+    B.EQ errorEnd
+
+    // -----------------------------------------------------------------
+	// CHECK IF CHAR == 'c' or CHAR == 'C' - CLEAR BINARY STRING
+	// -----------------------------------------------------------------
+    // LOWERCASE
+    CMP  W6, #'c'
+    B.EQ errorEnd
+
+    // UPPERCASE
+    CMP W6, #'C'
+    B.EQ errorEnd
+	// -----------------------------------------------------------------
+	// LOOP AGAIN
+	// -----------------------------------------------------------------
+	ADD X5, X5, #1	// X5++
+	B forEachChar
+
+	// -----------------------------------------------------------------
+	// OUTPUT INPUT
+	//	X3: X0, string
+	//	X4: X1, MAX_LENGTH
+	//	X5: counter
+	// -----------------------------------------------------------------
+output: 
+	STRB WZR, [X3, X5]  // X0[counter] = \0
+
+	MOV X0, STDOUT		// tells program we will output
+	MOV X1, X3			// string to output
+	MOV X2, X5			// number of characters to output
+	MOV X8, SYS_write	// Linux write() sys call
+	SVC 0				// call Linux to execute commands
+
+    // terminate program, encoun
+
+    // -----------------------------------------------------------------
+    // END PROGRAM - ENCOUNTER 'Q' OR ERROR
+    // -----------------------------------------------------------------
+errorEnd:
+    MOV X0, #0			// set return code to 0, all good 
+	MOV X8, #SYS_exit	// set exit() supervisor call code 
+	SVC 0				// call Linux to exit
+
+    // -----------------------------------------------------------------
+    // RETURN TO MAIN - NO ISSUES
+    // -----------------------------------------------------------------
+done:
+	RET     // return to main
+
+.end	// end of program, optional but good practice 
